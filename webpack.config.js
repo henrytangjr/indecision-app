818a7552fcca6fc25c7b015fcd86fc238ae97ada
Henry <henrytangjr@gmail.com>
2022-03-02 22:35:02 +0800

    Set up prod build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-  },
+module.exports = (env) => {
+  const isProduction = env === "production";
+
+  console.log("env", env);
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyAPIFallback: true,
+    },
+  };
 };
